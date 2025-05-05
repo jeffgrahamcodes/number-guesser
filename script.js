04de65ae9d72529326332ecfcc8418f457627a9e
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-05 11:02:39 -0400

    chore: add event listener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const msg = document.querySelector('.message');
+const input = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', () => {
+  const guess = Number(input.value);
+  if (!guess) {
+    msg.textContent = 'No number';
+  }
+});
